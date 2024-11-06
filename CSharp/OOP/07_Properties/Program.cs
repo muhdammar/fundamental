7c808b49a2795237eb478f592109badc4dfc86be
Muhammad Ammar <ammar.aziz.work@gmail.com>
2024-11-06 12:10:52 +0800

    07_Properties

---
+++ CSharp/OOP/07_Properties/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Xml.Linq;
+
+namespace _07_Properties
+{
+    public class Student
+    {
+        // Private fields
+        private int _id;
+        private string _name;
+        private double _passMark;
+
+        public void SetId(int Id)
+        {
+            if(Id <= 0)
+            {
+                throw new ArgumentException("Student Id should be greater than zero");
+            }   
+            this._id = Id;
+        }
+
+        public int GetId()
+        {
+            return this._id;
+        }
+
+        public void SetName(string Name)
+        {
+            if (string.IsNullOrEmpty(Name))
+            {
+                throw new Exception("Name cannot be null or empty");
+            }
+            this._name = Name;
+        }
+
+        public string GetName()
+        {
+            if(string.IsNullOrEmpty(this._name)) //can change to tertiary condition if u want
+            {
+                return "No Name";
+            }
+            return this._name;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Student C1 = new Student();
+            C1.SetId(1);
+            Console.WriteLine("Student Id = {0}", C1.GetId());
+            Console.WriteLine("Student Name = {0}", C1.GetName());
+            C1.SetName("Ammar");
+            Console.WriteLine("Student Name after change = {0}", C1.GetName());
+            //C1.SetName(null);//uncomment this to remove syntax error
+        }
+    }
+}
