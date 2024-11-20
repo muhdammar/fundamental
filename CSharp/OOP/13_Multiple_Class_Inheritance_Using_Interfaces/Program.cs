b0b7be72239e773b78b76e71b0a7beb7095f82f0
Muhammad Ammar <ammar.aziz.work@gmail.com>
2024-11-20 16:24:47 +0800

    multiple class inheritance using interfaces

---
+++ CSharp/OOP/13_Multiple_Class_Inheritance_Using_Interfaces/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _13_Multiple_Class_Inheritance_Using_Interfaces
+{
+    interface IA 
+    {
+        void AMethod();
+    }
+
+    class A : IA
+    {
+        public void AMethod()
+        {
+            Console.WriteLine("A");
+        }
+    }
+
+    interface IB
+    {
+        void BMethod();
+    }
+
+    class B : IB
+    {
+        public void BMethod()
+        {
+            Console.WriteLine("B");
+        }
+    }
+
+    class AB : IA, IB
+    {
+        A a = new A();
+        B b = new B();
+        public void AMethod()
+        {
+            a.AMethod();
+        }
+
+        public void BMethod()
+        {
+            b.BMethod();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AB ab = new AB();
+            ab.AMethod();
+            ab.BMethod();
+        }
+    }
+}
