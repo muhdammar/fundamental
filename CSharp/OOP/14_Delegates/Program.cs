bf5aeef4dee71bb17e8a34a4d5674f884dc1bcc9
Muhammad Ammar <ammar.aziz.work@gmail.com>
2024-11-20 23:44:24 +0800

    delegates

---
+++ CSharp/OOP/14_Delegates/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _14_Delegates
+{
+    public delegate void HelloFunctionDelegate(string Message);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //A delegate is a type safe function pointer
+
+            //this delegate is function pointer to "Hello" function
+            //HelloFunctionDelegate del = new HelloFunctionDelegate(Hello); //uncommented to see error
+            HelloFunctionDelegate del = new HelloFunctionDelegate(Hello2); //no syntax error
+            del("Hello from Delegate");
+        }
+        //this will return error to HelloFunctionDelegate since not match the return type
+        //thats why it called type safe pointer
+        public static string Hello(string text)
+        {
+            Console.WriteLine(text);
+            return text;
+        }
+        public static void Hello2(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ CSharp/OOP/15_Delegate_Usages_Examples/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Xml.Linq;
+
+namespace _15_Delegate_Usages_Examples
+{
+    class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public int Experience { get; set; }
+
+        public static void PromoteEmployee(List<Employee> employeeList) 
+        {
+            foreach (Employee employee in employeeList)
+            {
+                //this one not flexible/reusable logic
+                if (employee.Experience >= 5) { 
+                    Console.WriteLine(employee.Name + " promoted");
+                }
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+            employees.Add(new Employee(){ ID = 1, Name = "Ammar", Salary = 100, Experience = 6 });
+            employees.Add(new Employee(){ ID = 2, Name = "Abu", Salary = 100, Experience = 4 });
+            employees.Add(new Employee(){ ID = 2, Name = "Ali", Salary = 100, Experience = 7 });
+            employees.Add(new Employee(){ ID = 2, Name = "Azizi", Salary = 100, Experience = 3 });
+
+            Employee.PromoteEmployee(employees);
+        }
+    }
+}
