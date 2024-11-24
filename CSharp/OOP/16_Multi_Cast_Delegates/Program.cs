d99a831f359c2576ed5bcbea1613bb8429c6c737
Muhammad Ammar <ammar.aziz.work@gmail.com>
2024-11-24 12:33:25 +0800

    multi cast delegates

---
+++ CSharp/OOP/16_Multi_Cast_Delegates/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _16_Multi_Cast_Delegates
+{
+    public delegate void SampleDelegate();
+    internal class Program
+    {
+        public static void SampleMethodOne()
+        {
+            Console.WriteLine("SampleMethodOne invoked");
+        }
+
+        public static void SampleMethodTwo()
+        {
+            Console.WriteLine("SampleMethodTwo invoked");
+        }
+
+        public static void SampleMethodThree()
+        {
+            Console.WriteLine("SampleMethodThree invoked");
+        }
+
+        public static void SampleMethodFive()
+        {
+            Console.WriteLine("SampleMethodFour invoked");
+        }
+        static void Main(string[] args)
+        {
+            SampleDelegate del1, del2, del3, del4, del5;
+            del1 = new SampleDelegate(SampleMethodOne);
+            del2 = new SampleDelegate(SampleMethodTwo);
+            del3 = new SampleDelegate(SampleMethodThree);
+            del5 = new SampleDelegate(SampleMethodFive);
+
+            del4 = del1 + del2 + del3;
+            del4();
+
+            Console.WriteLine("\ndel5 remove from del4");
+            del4 = del4 - del5;//even we not add SampleMethodFive (del5) to del4, when we delete from del4,
+                               //no error, just it not remove anything
+            del4();
+        }
+    }
+}
