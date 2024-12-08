574917f6bb7028965abd0860cd5cbf328c1b9554
Muhammad Ammar <ammar.aziz.work@gmail.com>
2024-12-08 21:30:01 +0800

    Add Dependency Inversion

---
+++ CSharp/Design Pattern/DesignPatterns/5_Dependency_Inversion_Principle - BAD EXAMPLE/Program.cs
@@ -0,0 +1,37 @@
+﻿using DemoLibrary;
+
+namespace _5_Dependency_Inversion_Principle___BAD_EXAMPLE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // VIOLATION: High-level configuration happens here in Main,
+            // but all the dependencies are created inside the Chore class.
+            // This means we have no control over what logger or emailer is used.
+
+            Person owner = new Person
+            {
+                FirstName = "Tim",
+                LastName = "Corey",
+                EmailAddress = "tim@iamtimcorey.com",
+                PhoneNumber = "555-1212"
+            };
+
+            // The Chore class should receive its dependencies through constructor
+            // injection rather than creating them internally
+            Chore chore = new Chore
+            {
+                ChoreName = "Take out the trash",
+                Owner = owner
+            };
+
+            // When these methods are called, they create new instances of Logger
+            // and Emailer inside, violating DIP since we can't inject different
+            // implementations for testing or changing behavior
+            chore.PerformedWork(3);
+            chore.PerformedWork(1.5);
+            chore.CompleteChore();
+        }
+    }
+}
---
+++ CSharp/Design Pattern/DesignPatterns/5_Dependency_Inversion_Principle/Program.cs
@@ -0,0 +1,27 @@
+﻿using ConsoleUI;
+using DemoLibrary;
+
+namespace _5_Dependency_Inversion_Principle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPerson owner = Factory.CreatePerson();
+
+            owner.FirstName = "Tim";
+            owner.LastName = "Corey";
+            owner.EmailAddress = "tim@iamtimcorey.com";
+            owner.PhoneNumber = "555-1212";
+
+            IChore chore = Factory.CreateChore();
+            chore.ChoreName = "Take out the trash";
+            chore.Owner = owner;
+
+            chore.PerformedWork(3);
+            chore.PerformedWork(1.5);
+            chore.CompleteChore();
+
+        }
+    }
+}
