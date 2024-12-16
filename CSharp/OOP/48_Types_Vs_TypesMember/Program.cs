61962e6cbcd3aeed30ee564428d7a2957cc0305d
Muhammad Ammar <ammar.aziz.work@gmail.com>
2024-12-16 08:40:20 +0800

    access modifier

---
+++ CSharp/OOP/48_Types_Vs_TypesMember/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace _48_Types_Vs_TypesMember
+{
+
+    /// <summary>
+    /// this is "Types"
+    /// can have "internal" or "public"
+    /// cant have "private", "protected", "internal protected"
+    /// </summary>
+    public class CustomerAssemblyClassOne
+    {
+        /// <summary>
+        /// this is "Types Member"
+        /// 
+        /// procted must be access via derived class
+        /// </summary>
+        protected string Name { get; set; }
+
+        /// <summary>
+        /// it can be access in with other/same assembely if it derived it
+        /// </summary>
+        protected internal int Age { get; set; }
+
+        /// <summary>
+        /// access inside containing class
+        /// </summary>
+        private int BirthDay { get; set; }
+
+        /// <summary>
+        /// access inside assembly
+        /// </summary>
+        internal string LastName { get; set; }
+
+        /// <summary>
+        /// access to anywhere
+        /// </summary>
+        public int BirthMonth { get; set; }
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+    }
+    public class LoyalCustomer : CustomerAssemblyClassOne
+    {
+        public int Point { get; set; }
+        public void PrintPoint()
+        {
+            Console.WriteLine("Name :{0}, Point :{1}", Name, Point);
+        }
+    }
+    public class BadCustomer : CustomerAssemblyClassOne
+    {
+        public BadCustomer()
+        {
+            base.LastName = null;//can access becoz in same assembly (access via assembly only)
+            base.Name = "Ammar";//can access protected can access via derived class
+            base.Age = 1;
+        }
+    }
+
+    internal class CustomerAssemblyClassTwo
+    {
+        public int BirthDay { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerAssemblyClassOne customer = new CustomerAssemblyClassOne();
+            customer.Age = 1;
+            customer.LastName = "Aziz";
+            //customer.Name = "Test"; //sytanx error since proctected, u can access via derived class/ inside class
+
+            LoyalCustomer loyalCustomer = new LoyalCustomer();
+            loyalCustomer.SetName("Ammar");
+            loyalCustomer.PrintPoint();//access name in derived method
+        }
+    }
+}
---
+++ CSharp/OOP/49_Access_Modifiers/Program.cs
@@ -0,0 +1,29 @@
+﻿using _48_Types_Vs_TypesMember;
+
+namespace _49_Access_Modifiers
+{
+    public class SavingCustomer : CustomerAssemblyClassOne {
+        public SavingCustomer() {
+            //CAUTIONS SYNTAX ERROR
+            base.LastName = null;//cant access internal access modifier in "_48_Types_Vs_TypesMember" since it is internal (access via assembly only)
+            base.Age = 1; //can access this protected internal modifier from other assembly since it derived
+            base.Name = "Ammar";//can access protected via derived class even different accesemby
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //u can call this since it public
+            CustomerAssemblyClassOne customer = new CustomerAssemblyClassOne();
+
+            //CAUTIONS SYNTAX ERROR
+            customer.Age = 1; //u cant access age in customer since it protected internal, it can be access via derived class/in same assemlby
+            Console.WriteLine("Hello, World!");
+
+            //u cant call this class since it not available bcoz of internal in "_48_Types_Vs_TypesMember"
+            CustomerAssemblyClassTwo customer2 = new CustomerAssemblyClassTwo();
+        }
+    }
+}
