b211ef08bb8081cf99fff76a11a24d49730e2ca9
Muhammad Ammar <ammar.aziz.work@gmail.com>
2025-02-03 00:19:16 +0800

    78-83 Sort Complex list, Queue, & Stack

---
+++ CSharp/OOP/78_Sort_a_list_of_complex_types/Program.cs
@@ -0,0 +1,96 @@
+﻿namespace _78_Sort_a_list_of_complex_types
+{
+    internal class Program
+    {
+        /// <summary>
+        /// use c# IComparable interface
+        /// </summary>
+        public class Customer : IComparable<Customer>
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public int Salary { get; set; }
+
+            public int CompareTo(Customer obj)
+            {
+                //if (this.Salary > obj.Salary)
+                //    return 1;
+                //else if (this.Salary < obj.Salary)
+                //    return -1;
+                //else
+                //    return 0;
+
+                // OR, Alternatively you can also invoke CompareTo() method. 
+                return this.Salary.CompareTo(obj.Salary);
+            }
+        }
+
+
+        /// <summary>
+        /// use c# IComparer interface
+        /// </summary>
+        public class SortByName : IComparer<Customer>
+        {
+            public int Compare(Customer x, Customer y)
+            {
+                return x.Name.CompareTo(y.Name);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer()
+            {
+                ID = 101,
+                Name = "Mark",
+                Salary = 4000
+            };
+
+            Customer customer2 = new Customer()
+            {
+                ID = 102,
+                Name = "John",
+                Salary = 7000
+            };
+
+            Customer customer3 = new Customer()
+            {
+                ID = 103,
+                Name = "Ken",
+                Salary = 5500
+            };
+
+            List<Customer> listCutomers = new List<Customer>();
+            listCutomers.Add(customer1);
+            listCutomers.Add(customer2);
+            listCutomers.Add(customer3);
+
+            Console.WriteLine("Customers before sorting");
+            foreach (Customer customer in listCutomers)
+            {
+                Console.WriteLine(customer.Name + " - " + customer.Salary);
+            }
+
+            // Sort() method should sort customers by salary
+            listCutomers.Sort();
+
+            Console.WriteLine("Customers after sorting");
+            foreach (Customer customer in listCutomers)
+            {
+                Console.WriteLine(customer.Name + " - " + customer.Salary);
+            }
+
+            // To sort customers by name instead of salary
+            // SortByName sortByName = new SortByName(); --> from kudvenkat, i change to use interface follow SOLID principles
+            IComparer<Customer> sortByName = new SortByName();
+            listCutomers.Sort(sortByName);
+
+            Console.WriteLine("Customers after sorting by Name");
+            foreach (Customer customer in listCutomers)
+            {
+                Console.WriteLine(customer.Name + " - " + customer.Salary);
+            }
+        }
+    }
+}
---
+++ CSharp/OOP/80_useful_methods_of_List_collection_class/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace _80_useful_methods_of_List_collection_class
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Customer customer1 = new Customer()
+            {
+                ID = 101,
+                Name = "Mark",
+                Salary = 5200
+            };
+
+            Customer customer2 = new Customer()
+            {
+                ID = 103,
+                Name = "John",
+                Salary = 7000
+            };
+
+            Customer customer3 = new Customer()
+            {
+                ID = 102,
+                Name = "Ken",
+                Salary = 5500
+            };
+
+            List<Customer> listCutomers = new List<Customer>(100); //can change to 4 to see the different
+            listCutomers.Add(customer1);
+            listCutomers.Add(customer2);
+            listCutomers.Add(customer3);
+
+            Console.WriteLine("Are all salaries greater than 5000: "
+                + listCutomers.TrueForAll(x => x.Salary > 5000));
+
+            // ReadOnlyCollection will not have Add() or Remove() methods
+            System.Collections.ObjectModel.ReadOnlyCollection<Customer>
+                readOnlyCustomers = listCutomers.AsReadOnly();
+
+            Console.WriteLine("Total Items in ReadOnlyCollection = " +
+                readOnlyCustomers.Count);
+
+            // listCutomers list is created with an initial capacity of 100
+            // but only 3 items are in the list. The filled percentage is 
+            // less than 90 percent threshold.
+            // note from ammar: means if size is 4, it will not trim, since 3 / 4 is more than 90% threshold,
+            // so it will not trim and show 4
+            Console.WriteLine("List capacity before invoking TrimExcess = " +
+                    listCutomers.Capacity);
+            // Invoke TrimExcess() to set the capacity to the actual 
+            // number of elements in the List
+            listCutomers.TrimExcess();
+            Console.WriteLine(listCutomers.Capacity);
+        }
+    }
+
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+    }
+}
